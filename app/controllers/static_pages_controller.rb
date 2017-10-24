d954d85c498e65f01963ddd2805385af99a6af9c
Tobias Alldén <allden@Tobiass-MacBook-Pro.local>
2017-10-24 13:40:50 +0200

    Fixed skills

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
 	def about
-		@skills = Skill.all
+		@language_skills = Skill.where skill_type: 'language'
+		@other_skills = Skill.where skill_type: ['other','technical']
 	end
 end
