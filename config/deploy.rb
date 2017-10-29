b5ad36540f6887dffebb7d2e3d740650d756c551
Tobias Alldén <allden@Tobiass-MBP.lan>
2017-10-29 12:23:21 +0100

    Renamed

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, "PersonalWebpage"
-set :repo_url, "git@github.com:Tbbas/PersonalWepage.git"
+set :repo_url, "git@github.com:Tbbas/PersonalWebpage.git"
 
-set :deploy_to, '/home/allden/PersonalWebpage'
+set :deploy_to, '/home/deploy/PersonalWebpage'
 
 append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
